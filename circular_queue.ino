cdce45be82effe4eee280e3f9c506c760cdebdb5
jlzizmor <jlzizmor@wpi.edu>
2014-04-22 19:13:04 -0400

    added an init function to the circular queue

--- circular_queue.ino
+++ circular_queue.ino
@@ -11,3 +11,11 @@
  * This set of functions allows for the a float array to be implemented as a circular queue.
 */
 
+// this function initializes the circular queue
+// this function should be run once per queue at the beginning
+void initCQ(float cq) {
+	int size = sizeof(cq)/sizeof(float);
+	for (int i = 0; i < size; i++) {
+		cq[i] = 0;
+	}
+}
