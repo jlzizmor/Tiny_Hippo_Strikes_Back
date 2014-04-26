c434c3ca9fe4679c9c5aed06574e566493493fbc
jlzizmor <jlzizmor@wpi.edu>
2014-04-26 18:39:12 -0400

    finished code, debugging now

--- pot_to_pos.ino
+++ pot_to_pos.ino
@@ -18,5 +18,14 @@
 // output: the position at the current state
 int pot_to_pos(int ia, int ib, int xa, int xb, int pin) {
 	int del_i = abs(ib-ia);	// the change in input readings
-	int del_x = abs(xb-xa);
+	int del_x = abs(xb-xa);	// the change in position
+
+	float slope = ((float)del_x)-((float)del_i);	// the slope of the line
+													// in the form mx+b
+													// using known values of the pot can be used
+														// to make a formula relating an input
+														// to the position
+													// because there is no software offset,
+														// b == 0
+	return (int)(slope*analogRead(pin));
 }
