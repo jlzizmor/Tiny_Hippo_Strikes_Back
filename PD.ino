391d946bf99e468d513d20dd9d31083982e5c4cd
jlzizmor <jlzizmor@wpi.edu>
2014-04-28 16:58:28 -0400

    added PD control file

---
+++ PD.ino
@@ -0,0 +1,17 @@
+/* Jacob Zizmor
+ * Group 1
+ * Tiny Hippo
+ *
+ * RBE 2002
+ * D02 '14
+ * 
+ * This set of functions runs PD control.
+ *
+ * The functions for PD control are coded for controlling our eraser specifically
+ * because the robot will not be moving. If there were other parts moving, the code would
+ * have to be adjusted.
+*/
+
+void PDsample(int pin) {
+	
+}
