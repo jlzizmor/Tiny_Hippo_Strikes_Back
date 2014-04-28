d2be31ca14e8a91e5cddc891055ce0e15b1d5879
jlzizmor <jlzizmor@wpi.edu>
2014-04-28 01:04:13 -0400

    more added to main

--- main.ino
+++ main.ino
@@ -13,32 +13,38 @@
 #include <start_button_ISR>
 
 // all Servo objects
-Servo planetary;			// Servo which spins the planetary gears
-Servo hook;					// Servo which moves the whinch hook forward and back
-Servo eraser;				// Servo which moves the eraser into the robot and out of the robot
+Servo planetary;					// Servo which spins the planetary gears
+Servo hook;							// Servo which moves the whinch hook forward and back
+Servo eraser;						// Servo which moves the eraser into the robot and out
+									// of the robot
 
 // pins of each servo/motor
-int planetary_pin = -1;		// pin of the planetary Servo
-int hook_pin = -1;			// pin of the hook Servo
-int eraser_pin = 1;			// pin of the eraser Servo
+int planetary_pin = -1;				// pin of the planetary Servo
+int hook_pin = -1;					// pin of the hook Servo
+int eraser_pin = 1;					// pin of the eraser Servo
 
 // variables
-int eKp;		// the Kp value of the eraser
-int eKd;		// the Kd value of the eraser
-int eKi;		// the Ki value of the eraser
+int eKp;							// the Kp value of the eraser
+int eKd;							// the Kd value of the eraser
+int eKi;							// the Ki value of the eraser
 
 // pins
-int legTL = -1;			// pin to the pot of the top left foot
-int legTR = -1;			// pin to the pot of the top right foot
-int legBL = -1;			// pin to the pot of the bottom left foot
-int legBR = -1;			// pin to the pot of the bottom right foot
+int legTL = -1;						// pin to the pot of the top left foot
+int legTR = -1;						// pin to the pot of the top right foot
+int legBL = -1;						// pin to the pot of the bottom left foot
+int legBR = -1;						// pin to the pot of the bottom right foot
 
-int eraser_pot = -1;	// pin to the pot of the eraser
-int hook_pot = -1;		// pin to the pot of the hook
+int eraser_pot = -1;				// pin to the pot of the eraser
+int hook_pot = -1;					// pin to the pot of the hook
+int buttonISR = -1;					// pin to the button which starts the ISR
 
-int buttonISR = -1;		// pin to the button which starts the ISR
+int LED = -1;						// pin to the warning LED
 
-int LED = -1;			// pin to the warning LED
+// PID
+double eSetpoint;					// the goal value of the eraser PID
+double eInput;						// the input of the eraser PID
+double eOutput;						// the output of the eraser PID
+PID ePID(&)							// FIX*****************
 
 void setup() {
 	// attach all Servo objects
@@ -66,7 +72,7 @@ void setup() {
 															// than a small time delay
 															// **** -1 must be changed ****
 	// init PID
-	
+
 
 	hook_adjust(int legTL, int legTR, int legBL, int legBR); // adjust the position of the hook
 															 // to balance the robot on the
