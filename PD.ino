9ddf70750b802f22ed8593df039ee9c510d7c8ab
jlzizmor <jlzizmor@wpi.edu>
2014-04-28 17:57:52 -0400

    added functions (claculatePD(), P(), D())

--- PD.ino
+++ PD.ino
@@ -12,6 +12,27 @@
  * have to be adjusted.
 */
 
-void PDsample(int pin) {
-	
+int current = 0;						// the current read in of the sensor
+int last = 0;							// the last sensor value read
+
+int PD(int pin, int kp, int kd) {
+	last = current;						// set the previously read sample to the last variable
+	current = analogRead(pin);			// read in the new sample
+
+	return calculatePD(kp, kd);				// return the result of the PD calculation
+}
+
+int calculatePD(int kp, int kd) {
+	int error = calcE();				// calculate the error
+	float result = P(error, kp);		// result is equal to the sum of the P calculation and the D calculation
+	result = result + D(error, kd);		// displayed in 2 parts
+	return (int)result;					// return the integer value of the result, such that the Arduino can process it
+}
+
+float P(e, kp) {
+	return (float)kp*e;
+}
+
+float D(e, kp) {
+	return 
 }
