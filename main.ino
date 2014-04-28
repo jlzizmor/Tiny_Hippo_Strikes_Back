9f56c8927550ab6d894b97289cdac1069b1e5b38
jlzizmor <jlzizmor@wpi.edu>
2014-04-28 00:44:06 -0400

    added to main

--- main.ino
+++ main.ino
@@ -10,6 +10,7 @@
 
 #include PID_v1
 #include Servo.h
+#include <start_button_ISR>
 
 // all Servo objects
 Servo planetary;			// Servo which spins the planetary gears
@@ -27,10 +28,17 @@ int eKd;		// the Kd value of the eraser
 int eKi;		// the Ki value of the eraser
 
 // pins
-int legTL = -1;		// pin to the pot of the top left foot
-int legTR = -1;	// pin to the pot of the top right foot
-int legBL = -1;	// pin to the pot of the bottom left foot
-int legBR = -1;	// pin to the pot of the bottom right foot
+int legTL = -1;			// pin to the pot of the top left foot
+int legTR = -1;			// pin to the pot of the top right foot
+int legBL = -1;			// pin to the pot of the bottom left foot
+int legBR = -1;			// pin to the pot of the bottom right foot
+
+int eraser_pot = -1;	// pin to the pot of the eraser
+int hook_pot = -1;		// pin to the pot of the hook
+
+int buttonISR = -1;		// pin to the button which starts the ISR
+
+int LED = -1;			// pin to the warning LED
 
 void setup() {
 	// attach all Servo objects
@@ -39,7 +47,27 @@ void setup() {
 	eraser.attach(eraser_pin);								// attach the eraser motor
 
 	// pinMode
+	pinMode(legTL, INPUT);									// set all of the pots to INPUT
+	pinMode(legTR, INPUT);
+	pinMode(legBL, INPUT);
+	pinMode(legBR, INPUT);
+	pinMode(eraser_pot, INPUT);
+
+	pinMode(buttonISR, INPUT);								// set the button to INPUT
+
+	pinMode(LED, OUTPUT);									// set the LED to OUTPUT
+
+	// init ISR
+	attachInterrupt(-1, start_button_ISR, RISING);			// initialize ISR
+															// activates upon button press
+															// while the ISR will run if the
+															// button is pressed again, there
+															// will be no lasting effect other
+															// than a small time delay
+															// **** -1 must be changed ****
 	// init PID
+	
+
 	hook_adjust(int legTL, int legTR, int legBL, int legBR); // adjust the position of the hook
 															 // to balance the robot on the
 															 // current position of the board
