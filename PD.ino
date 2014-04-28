01110a613c75723c9e8d62bdcc61575b0168101e
jlzizmor <jlzizmor@wpi.edu>
2014-04-28 18:45:45 -0400

    filled out functions

--- PD.ino
+++ PD.ino
@@ -12,27 +12,43 @@
  * have to be adjusted.
 */
 
-int current = 0;						// the current read in of the sensor
-int last = 0;							// the last sensor value read
+int current_e = 0;						// the current read in of the sensor
+static int last_e = 0;					// the last error calculated
+int current_time = 0;					// the current time at which the sample is taken
+static int last_time = 0;				// the time at which the last sample was taken
 
+// controls all PD actions
+// also gathers all necesary information, makes not calculations
+// inputs:
+//	pin = pin to poll to get sample
+//	kp = proportional constant
+//	kd = derivative constant
+// output:
+//	the magnetude of which to drive the motor
 int PD(int pin, int kp, int kd) {
-	last = current;						// set the previously read sample to the last variable
-	current = analogRead(pin);			// read in the new sample
-
-	return calculatePD(kp, kd);				// return the result of the PD calculation
+	current_e = analogRead(pin);		// read in the new sample
+	current_time = millis();			// record the time at which the sample is taken
+										// although recording the time is not taken simultaniously to recording the sample
+										// because the time will be taken at the same point each time the method is run,
+										// with the same processes run before it, the time difference will still be the same.
+	return calculatePD(kp, kd);			// return the result of the PD calculation
 }
 
+// organizes all calculations for PD
 int calculatePD(int kp, int kd) {
 	int error = calcE();				// calculate the error
-	float result = P(error, kp);		// result is equal to the sum of the P calculation and the D calculation
+	int result = P(error, kp);			// result is equal to the sum of the P calculation and the D calculation
 	result = result + D(error, kd);		// displayed in 2 parts
-	return (int)result;					// return the integer value of the result, such that the Arduino can process it
+	last_e = error;						// record the last error calculated
+	last_time = current_time;			// recotd the last time taken
+	return result;						// return the integer value of the result, such that the Arduino can process it
 }
 
-float P(e, kp) {
+int P(e, kp) {
 	return (float)kp*e;
 }
 
-float D(e, kp) {
-	return 
+int D(e, kp) {
+	int del_e = e-last_e;				// delta e, change in error values
+	int del_t = current_time-last_time;	// delta t, change in time since last sample taken
 }
